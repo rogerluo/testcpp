4f670654062a64bd9ae51d2195339c7a801df912
rogerluo <luodingjia1985@163.com>
2013-10-10 08:40:42 +0800

    cpp

--- testSyntax/VirtualFunction.cpp
+++ testSyntax/VirtualFunction.cpp
@@ -8,11 +8,18 @@ void PrintVirutalFucAddr()
 	cout << "虚函数表地址：" << (int*)(&b1) << endl;
 	cout << "虚函数表 — 第一个函数地址：" << (int*)*(int*)(&b1) << endl;
 	typedef void (B1::*FUN)(void);
-	FUN pvfun = (int*)(*(int*)(&b1));
+
 	FUN pfun = &B1::f1;
 	(b1.*pfun)();
 	//cout << "执行第一个虚函数：" << (b1.*pfun)() << endl;
 	
-	
+	cout << "sizeof(double): " << sizeof(double) << endl;
+	cout << "sizeof(float): " << sizeof(float) << endl;
+	cout << "sizeof(unsigned long): " << sizeof(unsigned long) << endl;
+	cout << "sizeof(long): " << sizeof(long) << endl;
+	cout << "sizeof(unsigned int): " << sizeof(unsigned int) << endl;
+	cout << "sizeof(int): " << sizeof(int) << endl;
+	cout << "sizeof(unsigned short): " << sizeof(unsigned short) << endl;
+	cout << "sizeof(short): " << sizeof(short) << endl;
 }
 
