7ffa9e69df9208369bdcd5584689eaefcb757525
rogerluo <luodingjia1985@163.com>
2013-10-11 08:03:54 +0800

    cpp

--- testSyntax/VirtualFunction.cpp
+++ testSyntax/VirtualFunction.cpp
@@ -4,13 +4,26 @@
 
 void PrintVirutalFucAddr()
 {
-	B1 b1;
-	cout << "虚函数表地址：" << (int*)(&b1) << endl;
-	cout << "虚函数表 — 第一个函数地址：" << (int*)*(int*)(&b1) << endl;
-	typedef void (B1::*FUN)(void);
+	B1 b1(1), b2(2);
+	cout << "b1对象地址：" << &b1 << endl;
+	cout << "b2对象地址：" << &b2 << endl;
+	cout << "b1虚函数表地址：" << (int*)(&b1) << endl;
+	cout << "b2虚函数表地址：" << (int*)(&b2) << endl;
+	cout << "b1虚函数表 — 第一个函数地址：" << (int*)*(int*)(&b1) << endl;
+	cout << "b2虚函数表 — 第一个函数地址：" << (int*)*(int*)(&b2) << endl;
 
-	FUN pfun = &B1::f1;
-	(b1.*pfun)();
+	typedef void (B1::*FUN)(void);
+	typedef void (*VFUN)(int*);
+
+	//FUN pfun = &B1::f1;
+	//(b1.*pfun)();
+	VFUN pv1 = (VFUN)*((int*)*(int*)(&b1));
+	b1.v1();
+	pv1((int*)&b1);
+	VFUN pv2 = (VFUN)*((int*)*(int*)(&b1)+1);
+	pv2((int*)&b1);
+	VFUN pv3 = (VFUN)*((int*)*(int*)(&b1)+2);
+	pv3((int*)&b1);
 	//cout << "执行第一个虚函数：" << (b1.*pfun)() << endl;
 	
 	cout << "sizeof(double): " << sizeof(double) << endl;
--- testSyntax/VirtualFunction.h
+++ testSyntax/VirtualFunction.h
@@ -4,10 +4,13 @@ using namespace std;
 class B1
 {
 public:
-	virtual ~B1(){ cout << __FUNCTION__ <<endl; }
+	B1(int i):x(i){}
+	//~B1(){ cout << __FUNCTION__ <<endl; }
     int x;
-    virtual void v1(){ cout << "B1::v1" << endl; }
-    void f1(){cout << "B1::f1" << endl; }
+    virtual void v1(){ cout << "B1::v1 with " << x << endl; }
+	virtual void v2(){ cout << "B1::v2 with " << x << endl; }
+	virtual void v3(){ cout << "B1::v3 with " << x << endl; }
+    //void f1(){cout << "B1::f1 with " << x << endl; }
 };
 
 class B2
