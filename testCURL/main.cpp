0163f27a011bb6db688a3919469ccd0482c084d3
rogerluo <luodingjia1985@163.com>
2013-12-23 08:53:06 +0800

    asdf

--- testCURL/main.cpp
+++ testCURL/main.cpp
@@ -1,13 +1,21 @@
 #include "common.h"
 using namespace std;
-int GetHeader(const char * url)
-{
-	CURL* curl = curl_easy_init();
-	curl_easy_cleanup(curl);
-	return 0;
+__int64 GetHeaderLenth(const char *url){
+	double downloadFileLenth = 0;
+	CURL *handle = curl_easy_init();
+	curl_easy_setopt(handle, CURLOPT_URL, url);
+	curl_easy_setopt(handle, CURLOPT_HEADER, 1);    
+	curl_easy_setopt(handle, CURLOPT_NOBODY, 1);   
+	if (curl_easy_perform(handle) == CURLE_OK) {
+		curl_easy_getinfo(handle, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &downloadFileLenth);
+	} else {
+		downloadFileLenth = -1;
+	}
+	curl_easy_cleanup(handle);
+	return downloadFileLenth;
 }
 int main()
 {
-	GetHeader("");
+	cout<<GetHeaderLenth("http://www.confusedbycode.com/curl/")<<endl;
 	return 0;
 }
