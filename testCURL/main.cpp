db40178e18ab809b004a3a85b66ed360bb57a0dc
rogerluo <luodingjia1985@163.com>
2014-01-16 18:52:57 +0800

    asdf

--- testCURL/main.cpp
+++ testCURL/main.cpp
@@ -4,8 +4,8 @@ __int64 GetHeaderLenth(const char *url, const char *proxy = NULL, bool header = 
 	double downloadFileLenth = 0;
 	CURL *handle = curl_easy_init();
 	curl_easy_setopt(handle, CURLOPT_URL, url);
-	curl_easy_setopt(handle, CURLOPT_HEADER, 1);    
-	curl_easy_setopt(handle, CURLOPT_NOBODY, 1);   
+	curl_easy_setopt(handle, CURLOPT_HEADER, 1L);    
+	curl_easy_setopt(handle, CURLOPT_NOBODY, 1L);   
 	curl_easy_setopt(handle, CURLOPT_TIMEOUT_MS, 1000);
 	if (verbose)
 		curl_easy_setopt(handle, CURLOPT_VERBOSE, 1L);
@@ -27,11 +27,10 @@ __int64 GetHeaderLenth(const char *url, const char *proxy = NULL, bool header = 
 }
 int main()
 {
-	cout<<GetHeaderLenth("http://localhost/std/summary.ini", nullptr, true, true)<<endl;
-	cout<<GetHeaderLenth("http://localhost/std/summary_2.ini")<<endl;
-	cout<<GetHeaderLenth("http://localhost/std/summary_3.ini")<<endl;
+	cout<<GetHeaderLenth("http://10.35.21.201/std/summary.ini")<<endl;
 	cout<<GetHeaderLenth("http://www.confusedbycode.com/curl/", "http://10.40.14.55:80")<<endl;
-	cout<<GetHeaderLenth("http://www.confusedbycode.com/curl/", nullptr, true, true)<<endl;
+	cout<<GetHeaderLenth("http://www.confusedbycode.com/curl/")<<endl;
+	cout<<GetHeaderLenth("http://introcs.cs.princeton.edu/java/data", "http://10.40.14.55:80")<<endl;
 
 	return 0;
 }
